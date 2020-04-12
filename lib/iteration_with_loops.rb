3ed2493a997923d293267bb6f79c89611e5daee7
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-04-12 14:23:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,9 @@ row_index = 0
 while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
+    if src[row_index][element_index].class == String
     sentence += src[row_index][element_index]
+  end
     element_index += 1
   end
   row_index += 1
